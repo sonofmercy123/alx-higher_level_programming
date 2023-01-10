b285c4edee37bfe7734eea1abb2f315a8b9b0662
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2023-01-10 20:24:28 +0100

    Create 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+to_json_string function
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    Returns the JSON representation of an object
+    '''
+    return (json.dumps(my_obj))
