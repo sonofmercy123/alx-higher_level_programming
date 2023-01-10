47649a5c1b6ad8bb71def44e5f92fd7771e3a445
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2023-01-10 20:46:49 +0100

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+class Student
+'''
+
+
+class Student:
+    '''
+    class that defines a student by first_name, last_name, and age
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
+
+    def reload_from_json(self, json):
+        self.__dict__.update(json)
