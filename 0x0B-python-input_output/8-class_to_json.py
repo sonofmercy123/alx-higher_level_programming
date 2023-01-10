8343eb1f618bcfc57c5efb10bf834ade69f38213
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2023-01-10 20:40:37 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''
-to_json_string function
+class_to_json function
 '''
-import json
 
 
-def to_json_string(my_obj):
+def class_to_json(obj):
     '''
-    Returns the JSON representation of an object
+    Returns the dictionary description with simple data structure for
+    JSON serialization of an object
     '''
-    return (json.dumps(my_obj))
+    return obj.__dict__
